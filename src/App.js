5866bfaa3a04a597efe9037a61deaaee15de3b1e
Sin PRop <Sinproper@gmail.com>
2022-01-30 20:08:54 -0500

    made most progress on todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault();
+
+    if (newTodo.length === 0) {
+      return;
+    }
+
+    const todoItem = {
+      text: newTodo,
+      complete: false
+    };
+
+    setTodos([...todos, todoItem])
+    setNewTodo("");
+  };
+
+  const handleTodoDelete = (deleteIndex) => {
+    const filteredTodos = todos.filter((_todo, i) =>{
+      return i !== deleteIndex;
+    });
+
+    setTodos(filteredTodos);
+  }
+
+const handleToggleComplete = (index) => {
+  const updatedTodos = todos.map((todo, i) =>{
+    if (index === i) {
+      todo.complete = !todo.complete;
+    }
+    return todo;
+  });
+
+  setTodos(updatedTodos);
 }
+
+  return (
+    <div style={{ textAlign: "center"}}>
+      <form onSubmit={(event) => {
+      handleNewTodoSubmit(event);
+    }}>
+        <input onChange={(event) =>{
+          setNewTodo(event.target.value);
+        }} 
+          type="text"
+          value={newTodo}
+          />
+          <div>
+            <button>Add</button>
+          </div>
+      </form>
+
+<hr />
+
+      {todos.map((todo, i) =>{
+          return (
+          <div key={i}>
+            <span>{todo.text}</span>
+            <input onChange={(event) => {
+              handleToggleComplete(i);
+
+            }} checked={todo.complete} type="checkbox" />
+            <button onClick={(event)=>{
+              handleTodoDelete(i);
+            }
+            }style={{marginLeft: "15px"}}>Delete</button>
+          </div>
+    );
+})}
+</div>
+  );
+};
 
 export default App;
